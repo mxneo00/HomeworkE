d02369a4de911df5095568c3cee8b71049a1dbaf
Katellyn Hyker <llyn00@yahoo.com>
2025-08-25 19:11:53 -0400

    Completed findMaxInteger function

--- search-module-student.c
+++ search-module-student.c
@@ -11,15 +11,28 @@ static int findMaxInteger(int list[], int startingIndex, int numbrOfEntries);
 // ################################################################
 void searchIntegerList(int list[], int nbrOfListEntries, int childrenCount)
 {
+    for (int i = 0; i < childrenCount; i++){
 
+    }
+    
 } // End searchIntegerList
 
 
 // ################################################################
 static int findMaxInteger(int list[], int startingIndex, int nbrOfEntries)
 {
+    // Set intial value for maxInt
+    int maxInt = list[startingIndex];
 
-return 0;
+    // Overall for loop to loop through all entries
+    for (int i = 1; i < nbrOfEntries; i++){
+        // If loop to determine if next value is larger than the maxInt
+        if(list[startingIndex +1] > maxInt){
+            // If true maxInt is set to the next value 
+            maxInt = list[startingIndex + i];
+        }
+    }
+    return maxInt;
 } // End findMaxInteger
 
 

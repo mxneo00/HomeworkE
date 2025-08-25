db006b3ea9284881e9edb1733976f8c1b2fe66fa
Katellyn Hyker <87871440+mxneo00@users.noreply.github.com>
2025-08-25 12:14:01 -0400

    Base files

---
+++ search-module-student.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "listSearcher.h"
+
+// Function Prototypes
+static int findMaxInteger(int list[], int startingIndex, int numbrOfEntries);
+
+
+// ################################################################
+void searchIntegerList(int list[], int nbrOfListEntries, int childrenCount)
+{
+
+} // End searchIntegerList
+
+
+// ################################################################
+static int findMaxInteger(int list[], int startingIndex, int nbrOfEntries)
+{
+
+return 0;
+} // End findMaxInteger
+
+
